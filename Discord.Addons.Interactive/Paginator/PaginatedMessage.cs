a94833e0d58ebe2120091f48aa44c366928c2899
Matthew Trip <matthewtrip2@gmail.com>
2020-07-06 10:46:18 +0100

    Create EmbedPage

--- Discord.Addons.Interactive/Paginator/PaginatedMessage.cs
+++ Discord.Addons.Interactive/Paginator/PaginatedMessage.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+using System;
+using System.Collections.Generic;
 
 namespace Discord.Addons.Interactive.Paginator
 {
@@ -36,5 +37,36 @@ namespace Discord.Addons.Interactive.Paginator
         public string AlternateDescription { get; set; } = "";
 
         public PaginatedAppearanceOptions Options { get; set; } = PaginatedAppearanceOptions.Default;
+    }
+
+    public class EmbedPage
+    {
+        public string AlternateAuthorTitle { get; set; }
+        
+        public string AlteranteAuthorIcon { get; set; }
+        
+        public bool DisplayTotalFieldsCount { get; set; } = false;
+        
+        public string TotalFieldsMessage { get; set; } = null;
+        
+        public double TotalFieldsCountConstant { get; set; } = 1;
+        
+        public string Title { get; set; }
+        
+        public string Url { get; set; } = null;
+        
+        public string Description { get; set; }
+        
+        public string ImageUrl { get; set; }
+        
+        public string ThumbnailUrl { get; set; } = null;
+        
+        public List<EmbedFieldBuilder> Fields { get; set; } = new List<EmbedFieldBuilder>();
+        
+        public EmbedFooterBuilder FooterOverride { get; set; } = null;
+        
+        public DateTimeOffset? TimeStamp { get; set; } = null;
+        
+        public Color? Color { get; set; } = null;
     }
 }
