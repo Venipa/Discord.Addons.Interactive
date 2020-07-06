bf6b101e224b4be81b2e00017cbb3d749e6c8889
Matthew Trip <matthewtrip2@gmail.com>
2020-07-06 10:44:02 +0100

    Cleaning

--- SampleApp/Modules/SampleModule.cs
+++ SampleApp/Modules/SampleModule.cs
@@ -1,4 +1,4 @@
-using Discord;
+﻿using Discord;
 using Discord.Addons.Interactive;
 using Discord.Addons.Interactive.InlineReaction;
 using Discord.Commands;
@@ -11,7 +11,7 @@ namespace SampleApp.Modules
 {
     public class SampleModule : InteractiveBase
     {
-        // DeleteAfterAsync will send a message and asynchronously delete it after the timeout has popped
+        // DeleteAfterAsync will send a message and asynchronously delete it after the timeout has popped.
         // This method will not block.
         [Command("delete")]
         public async Task<RuntimeResult> Test_DeleteAfterAsync()
@@ -21,17 +21,22 @@ namespace SampleApp.Modules
         }
 
         // NextMessageAsync will wait for the next message to come in over the gateway, given certain criteria
-        // By default, this will be limited to messages from the source user in the source channel
+        // By default, this will be limited to messages from the source user in the source channel.
         // This method will block the gateway, so it should be ran in async mode.
         [Command("next", RunMode = RunMode.Async)]
         public async Task Test_NextMessageAsync()
         {
             await ReplyAsync("What is 2+2?");
+            
             var response = await NextMessageAsync();
             if (response != null)
+            {
                 await ReplyAsync($"You replied: {response.Content}");
+            }
             else
+            {
                 await ReplyAsync("You did not reply before the timeout");
+            }
         }
 
         // PagedReplyAsync will send a paginated message to the channel
@@ -85,15 +90,14 @@ namespace SampleApp.Modules
 
         // InlineReactionReplyAsync will send a message and adds reactions on it.
         // Once an user adds a reaction, the callback is fired.
-        // If callback was successful next callback is not handled
+        // If callback was successful next callback is not handled.
         // Unsuccessful callback is a reaction that did not have a callback.
         [Command("reaction")]
         public async Task Test_ReactionReply()
         {
             await InlineReactionReplyAsync(new ReactionCallbackData("text", null, false, false)
                 .WithCallback(new Emoji("👍"), (c, r) => c.Channel.SendMessageAsync($"{r.User.Value.Mention} replied with 👍"))
-                .WithCallback(new Emoji("👎"), (c, r) => c.Channel.SendMessageAsync($"{r.User.Value.Mention} replied with 👎"))
-            );
+                .WithCallback(new Emoji("👎"), (c, r) => c.Channel.SendMessageAsync($"{r.User.Value.Mention} replied with 👎")));
         }
     }
 }
